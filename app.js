1bae058e3c136b5637f9f2a8b282653aa6d4edc7
Artem Tsymbaliuk <abc.artem.abc@gmail.com>
2022-01-26 19:32:40 +0200

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/index.js";
 import bodyParser from 'body-parser'
-import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
@@ -11,7 +10,7 @@ const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@$
 const port = process.env.PORT || 3000
 
 /*app.use(express.json());*/
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(router);
